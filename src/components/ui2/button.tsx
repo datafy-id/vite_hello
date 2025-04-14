526fdfc5dd34f816b2b7a0a6014e50396b51d828
Muhammad Ridho <reedho@gmail.com>
2025-04-14 07:48:58 +0700

    Add manually components from jollyui to components/ui2

---
+++ src/components/ui2/button.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { cva, type VariantProps } from "class-variance-authority"
+import {
+  Button as AriaButton,
+  composeRenderProps,
+  type ButtonProps as AriaButtonProps,
+} from "react-aria-components"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  [
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors",
+    /* Disabled */
+    "data-disabled:pointer-events-none data-disabled:opacity-50 ",
+    /* Focus Visible */
+    "data-focus-visible:outline-hidden data-focus-visible:ring-1 data-focus-visible:ring-ring ",
+    /* Resets */
+    "focus-visible:outline-hidden",
+  ],
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow-sm data-hovered:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-xs data-hovered:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-xs  data-hovered:bg-accent data-hovered:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-xs data-hovered:bg-secondary/80",
+        ghost: "data-hovered:bg-accent data-hovered:text-accent-foreground",
+        link: "text-primary underline-offset-4 data-hovered:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+interface ButtonProps
+  extends AriaButtonProps,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = ({ className, variant, size, ...props }: ButtonProps) => {
+  return (
+    <AriaButton
+      className={composeRenderProps(className, (className) =>
+        cn(
+          buttonVariants({
+            variant,
+            size,
+            className,
+          })
+        )
+      )}
+      {...props}
+    />
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { Button, buttonVariants }
+export type { ButtonProps }
